a467e3f63d38dc6657070f8ebf7d5c25a9fb7787
Rahulk005 <rs13009406@gmail.com>
2024-10-08 10:12:23 +0000

    save

---
+++ LearnJS/02-datatypes.js
@@ -0,0 +1,2 @@
+"use stricts"; // treat all JS code as newer version //
+alert (5+5)  //we are using nodeJS, not browser //
---
+++ LearnJS/variable01.js
@@ -0,0 +1,19 @@
+const accountId = 13005
+
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "123456789"
+accountCity = "chandigarh"
+let accountNumber;
+
+console.log(accountId);
+
+accountEmail = "rahul0222@gmailcom"
+accountPassword = "15975314"
+accountCity = "Himachal"
+/*Not use var bcz of an issue in block scope & ffunctional sope*/
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountNumber]);
+
+
+
+
