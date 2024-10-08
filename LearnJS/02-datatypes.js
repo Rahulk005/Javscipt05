d101e1ffe2d20983d91baf27c4b3ccee9e77c8f6
Rahulk005 <rs13009406@gmail.com>
2024-10-08 10:41:07 +0000

    save datatype

--- LearnJS/02-datatypes.js
+++ LearnJS/02-datatypes.js
@@ -1,2 +1,29 @@
 "use stricts"; // treat all JS code as newer version //
 alert (5+5)  //we are using nodeJS, not browser //
+
+
+console.log(4 
+    +
+     3) // code readability should be high
+
+console.log("Rahul")
+
+
+let name = "Rahul"
+let age = 15
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
